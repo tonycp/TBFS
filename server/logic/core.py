bc284e3707408420a4f9fa04f3da4e9520f73f02
tonycp <tonycadahia@gmail.com>
2025-02-15 12:16:31 -0500

    upgrade handlers

--- server/logic/core.py
+++ server/logic/core.py
@@ -1,4 +1,4 @@
-from typing import Callable, Any, Dict, List, Optional, Union
+from typing import Callable, Any, Dict, List, Optional, Union, Tuple
 from dotenv import load_dotenv
 import os, time, zmq, json, logging
 from .handlers import handlers
@@ -23,42 +23,19 @@ def _check_default(
     return config
 
 
-def _load_handler(header_str: str) -> Optional[Callable[[Dict[str, Any]], Any]]:
-    """Load the appropriate handler based on the header."""
+# Parse the header string and return the command name, function name, and dataset.
+def parse_header(header_str: str) -> None:
+    """Parse the header string and return the command name, function name, and dataset."""
     if not header_str:
         raise ValueError("Header is empty")
 
     header: Dict[str, Any] = json.loads(header_str)
 
-    # Verificar que las claves necesarias estén presentes
     command_name = header.get("command_name")
     func_name = header.get("function")
     dataset = header.get("dataset")
 
-    if command_name is None or dataset is None:
-        raise ValueError("Missing command_name or dataset in header")
-    args = ":?".join(dataset) + ":?"
-    handler_key = f"{command_name}//{func_name}//{args}"
-    handler = handlers.get(handler_key)
-
-    if not handler:
-        raise ValueError("Unknown command name or dataset")
-
-    return handler_key, handler[0], handler[1]  # Retornar solo la función y el dataset
-
-
-def _load_data(data: List[bytes], dataset: Dict[str, Any]) -> Dict[str, Any]:
-    """Load the data from the message into a dictionary."""
-    data_dict: Dict[str, Any] = json.loads(data[0].decode("utf-8"))
-    result = {}
-    for key, value_type in dataset.items():
-        value = data_dict.get(key)
-        if type(value) != dict:
-            result[key] = value_type(value)
-        else:
-            result[key] = value_type(**value)
-
-    return result
+    return command_name, func_name, dataset
 
 
 def start_listening() -> None:
@@ -88,18 +65,10 @@ def start_listening() -> None:
                     last_endpoint = socket.getsockopt(zmq.LAST_ENDPOINT).decode("utf-8")
                     logging.info(f"Received a message from: {last_endpoint}")
 
-                    handler_name, handler_func, dataset = _load_handler(
-                        message[0].decode("utf-8")
-                    )
-                    logging.info(f"Redirecting to handler: {handler_name}")
-
-                    incoming_data = _load_data(message[1:], dataset)
-                    logging.info(f"checked incoming data: {incoming_data}")
-
-                    result = handler_func(**incoming_data)
+                    result = handle_request(parse_header(message[0].decode("utf-8")))
                     logging.info(f"Result: {result}")
 
-                    socket.send_multipart([json.dumps(result).encode("utf-8")])
+                    socket.send_multipart([result.encode("utf-8")])
                     logging.info(f"Response sent to: {last_endpoint}")
                 except ValueError as e:
                     logging.error(f"Error processing message: {e}")
--- server/logic/handlers.py
+++ server/logic/handlers.py
@@ -1,51 +1,110 @@
 import json
-from typing import Callable, Any, Dict, Optional
+import logging
+from typing import Callable, Any, Dict, Optional, Tuple, List
 
-handlers = {}  # Dict[str, (Callable[[Dict[str, Any]], Any], Dict[str, Any])]
+handlers: Dict[
+    str, Tuple[Callable[[Dict[str, Any]], str], Dict[str, Callable[[Any], bool]]]
+] = {}
 
 
-def create_decorator(
-    command_name: str, dataset: Dict[str, Any]
-) -> Callable[[Callable[..., Any]], Callable[..., None]]:
-    """Generic decorator factory for command handlers."""
+def _load_data(
+    data: List[bytes], dataset: Dict[str, Callable[[Any], bool]]
+) -> Dict[str, Any]:
+    """Load the data from the message into a dictionary."""
+    try:
+        data_dict: Dict[str, Any] = json.loads(data[0].decode("utf-8"))
+    except (IndexError, json.JSONDecodeError) as e:
+        raise ValueError(f"Invalid data format: {e}")
+
+    result = {}
+    for key, value_type in dataset.items():
+        value = data_dict.get(key)
+        if value is None:
+            raise ValueError(f"Missing required key: {key}")
+        try:
+            if isinstance(value, dict):
+                result[key] = value_type(**value)
+            else:
+                result[key] = value_type(value)
+        except Exception as e:
+            raise ValueError(f"Error processing key '{key}': {e}")
+
+    return result
+
+
+def handle_request(header: Tuple[str, str, Dict[str, Any]]) -> Any:
+    """Handle incoming requests and route them to the appropriate handler."""
+    try:
+        command_name, func_name, data = header
+        if command_name is None or func_name is None:
+            raise ValueError("Missing command_name or func_name in header")
+
+        args = ":?".join(data.keys()) + ":?"
+        handler_key = f"{command_name}//{func_name}//{args}"
+        handler = handlers.get(handler_key)
+
+        if not handler:
+            raise ValueError("Unknown command name or dataset")
+
+        handler_func, dataset = handler
+        return handler_func(**_load_data(data, dataset))
+    except Exception as e:
+        logging.error(f"Error handling request: {e}")
+        return {"error": str(e)}
+
+
+def create_handler(
+    command_name: str, dataset: Dict[str, Callable[[Any], bool]]
+) -> Callable[[Callable[..., Any]], Callable[..., str]]:
+    """Generic handler factory for commands."""
     global handlers
 
-    def decorator(
-        func: Callable[[Dict[str, Any], Any], Any]
-    ) -> Callable[[Dict[str, Any], Any], None]:
+    def handler(func: Callable[..., Any]) -> Callable[..., str]:
         args = ":?".join(dataset.keys()) + ":?"
         index = f"{command_name}//{func.__name__}//{args}"
         handlers[index] = (func, dataset)
-        return func
 
-    return decorator
+        def wrapper(data: Dict[str, Any]) -> str:
+            try:
+                validated_errors = [k for k, v in dataset.items() if not v(data[k])]
+                if validated_errors:
+                    raise ValueError(f"Invalid data: {validated_errors}")
+
+                result = func(**data)
+                return json.dumps(result)
+            except Exception as e:
+                return json.dumps({"error": str(e)})
+
+        return wrapper
+
+    return handler
 
 
 def Create(
-    dataset: Dict[str, Optional[Any]]
-) -> Callable[[Callable[..., Any]], Callable[..., None]]:
-    return create_decorator("Create", dataset)
+    dataset: Dict[str, Optional[Callable[[Any], bool]]]
+) -> Callable[[Callable[..., Any]], Callable[..., str]]:
+    return create_handler("Create", dataset)
 
 
 def Update(
-    dataset: Dict[str, Optional[Any]]
-) -> Callable[[Callable[..., Any]], Callable[..., None]]:
-    return create_decorator("Update", dataset)
+    dataset: Dict[str, Optional[Callable[[Any], bool]]]
+) -> Callable[[Callable[..., Any]], Callable[..., str]]:
+    return create_handler("Update", dataset)
 
 
 def Delete(
-    dataset: Dict[str, Optional[Any]]
-) -> Callable[[Callable[..., Any]], Callable[..., None]]:
-    return create_decorator("Delete", dataset)
+    dataset: Dict[str, Optional[Callable[[Any], bool]]]
+) -> Callable[[Callable[..., Any]], Callable[..., str]]:
+    return create_handler("Delete", dataset)
 
 
 def Get(
-    dataset: Dict[str, Optional[Any]]
-) -> Callable[[Callable[..., Any]], Callable[..., None]]:
-    return create_decorator("Get", dataset)
+    dataset: Dict[str, Optional[Callable[[Any], bool]]]
+) -> Callable[[Callable[..., Any]], Callable[..., str]]:
+    return create_handler("Get", dataset)
 
 
 def GetAll(
-    dataset: Dict[str, Optional[Any]]
-) -> Callable[[Callable[..., Any]], Callable[..., None]]:
-    return create_decorator("GetAll", dataset)
+    dataset: Dict[str, Optional[Callable[[Any], bool]]]
+) -> Callable[[Callable[..., Any]], Callable[..., str]]:
+    return create_handler("GetAll", dataset)
